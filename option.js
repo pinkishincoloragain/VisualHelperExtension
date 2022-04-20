4259478347f3fc7f95ea478dbba4c7013419e79e
pinkishincoloragain <smb1103@gmail.com>
2022-04-20 15:11:56 +0100

    making react version

--- option.js
+++ option.js
@@ -4,23 +4,21 @@ const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
 
 // Reacts to a button click by marking the selected button and saving
 // the selection
-function handleButtonClick(event) {
+const handleButtonClick = (e) => {
   // Remove styling from the previously selected color
-  let current = event.target.parentElement.querySelector(
-    `.${selectedClassName}`
-  );
-  if (current && current !== event.target) {
+  let current = e.target.parentElement.querySelector(`.${selectedClassName}`);
+  if (current && current !== e.target) {
     current.classList.remove(selectedClassName);
   }
 
   // Mark the button as selected
-  let color = event.target.dataset.color;
-  event.target.classList.add(selectedClassName);
+  let color = e.target.dataset.color;
+  e.target.classList.add(selectedClassName);
   chrome.storage.sync.set({ color });
-}
+};
 
 // Add a button to the page for each supplied color
-function constructOptions(buttonColors) {
+const constructOptions = (buttonColors) => {
   chrome.storage.sync.get("color", (data) => {
     let currentColor = data.color;
     // For each color we were provided…
@@ -40,7 +38,7 @@ function constructOptions(buttonColors) {
       page.appendChild(button);
     }
   });
-}
+};
 
 // Initialize the page by constructing the color options
 constructOptions(presetButtonColors);
